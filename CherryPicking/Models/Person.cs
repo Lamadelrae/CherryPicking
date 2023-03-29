9fb2e70aa2844219334e9ccb6f5146dba334bb0d
Lamadelrae <matthew.almeida572@gmail.com>
2023-03-29 19:38:17 -0300

    feature: adding data

--- CherryPicking/Models/Person.cs
+++ CherryPicking/Models/Person.cs
@@ -12,6 +12,9 @@ namespace CherryPicking.Models
         public string Name { get; set; }
         public string Email { get; set; }
 
+        public string? Address { get; set; }
+        public string? City { get; set; }
+
         public Person(int id, string name, string email)
         {
             ArgumentException.ThrowIfNullOrEmpty(name);
