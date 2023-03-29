918ede9f00e8fc61f70da94d982d6b815655f83a
Lamadelrae <matthew.almeida572@gmail.com>
2023-03-29 19:39:12 -0300

    feature: removing useless usings

--- CherryPicking/Models/Person.cs
+++ CherryPicking/Models/Person.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CherryPicking.Models
+﻿namespace CherryPicking.Models
 {
     public class Person
     {
