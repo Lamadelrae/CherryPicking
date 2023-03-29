f39bd76948287c89aa35a324a7559a56086cb750
Lamadelrae <matthew.almeida572@gmail.com>
2023-03-29 19:37:29 -0300

    feature: adding constructor validation

--- CherryPicking/Models/Person.cs
+++ CherryPicking/Models/Person.cs
@@ -12,9 +12,12 @@ namespace CherryPicking.Models
         public string Name { get; set; }
         public string Email { get; set; }
 
-
         public Person(int id, string name, string email)
         {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentException.ThrowIfNullOrEmpty(email);
+            if (id <= 0) throw new ArgumentOutOfRangeException("id");
+
             Id = id;
             Name = name;
             Email = email;
